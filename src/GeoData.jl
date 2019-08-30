22a7dcc4bd9899db29d1f15a847491c93bf74522
Rafael Schouten <rafaelschouten@gmail.com>
2019-08-30 15:49:25 +1000

    define dims in GeoData

--- src/GeoData.jl
+++ src/GeoData.jl
@@ -4,7 +4,7 @@ using Mixers, RecipesBase, Reexport, Requires
 @reexport using CoordinateReferenceSystemsBase, DimensionalData
 
 
-using DimensionalData: Time, formatdims, slicedims, basetype, dims2indices
+using DimensionalData: Time, formatdims, slicedims, basetype, dims2indices, @dim
 
 import DimensionalData: val, dims, refdims, metadata, rebuild, select, selectview
 import CoordinateReferenceSystemsBase: crs
@@ -12,7 +12,12 @@ import CoordinateReferenceSystemsBase: crs
 export AbstractGeoArray, GeoArray
 export AbstractGeoStack, GeoStack
 export AbstractGeoSeries, GeoSeries
-export coordinates, coordinates!, missingval, metadata, replace_missing
+export missingval, metadata, mask, replace_missing
+
+@dim Lon "Longitude"
+@dim Lat "Latitude"
+@dim Vert "Vertical"
+@dim Band
 
 include("interface.jl")
 include("array.jl")
@@ -21,8 +26,6 @@ include("series.jl")
 include("coordinates.jl")
 include("plotrecipes.jl")
 include("utils.jl")
-
-DimensionalData.@dim Band
 
 # function __init__()
     # @require HDF5="f67ccb44-e63f-5c2f-98bd-6dc0ccc4ba2f" begin
