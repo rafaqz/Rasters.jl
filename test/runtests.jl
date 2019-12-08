6e05f0990b2d86c7b7a34442d5ae4a7fca64f804
Rafael Schouten <rafaelschouten@gmail.com>
2019-12-08 14:38:50 +1100

    dont test gdal on windows for now

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,8 @@
 include("array.jl")
 include("stack.jl")
 include("series.jl")
-include("gdal.jl")
+# GDAL Environment vars needs to be set manually for windows, so skip for now
+Sys.iswindows() || include("gdal.jl")
 include("ncdatasets.jl")
 include("smap.jl")
 include("grd.jl")
