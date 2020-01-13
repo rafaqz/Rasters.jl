04b57abb55993d02aeebba1b9cc084d90a2dad73
Rafael Schouten <rafaelschouten@gmail.com>
2020-01-14 10:53:49 +1100

    fix window application from series

--- src/series.jl
+++ src/series.jl
@@ -2,8 +2,8 @@
 An AbstractGeoArray that holds or points to a series of stacks.
 
 This is useful abstraction where data is broken into separate
-files accross one or more dimensions, and need to be loaded 
-separately. 
+files accross one or more dimensions, and need to be loaded
+separately.
 """
 abstract type AbstractGeoSeries{T,N,D,CT} <: AbstractGeoArray{T,N,D} end
 
@@ -17,11 +17,11 @@ window(A::AbstractGeoSeries) = A.window
 # Array interface methods ##############################################
 # Mostly these inherit from AbstractGeoArray
 
-Base.getindex(A::AbstractGeoSeries{<:AbstractString}, I::Vararg{<:Integer}) = 
-    childtype(A)(data(A)[I...]; dims=childdims(A), refdims=slicedims(A, I)[2], 
+Base.getindex(A::AbstractGeoSeries{<:AbstractString}, I::Vararg{<:Integer}) =
+    childtype(A)(data(A)[I...]; dims=childdims(A), refdims=slicedims(A, I)[2],
                  window=window(A), metadata=metadata(A))
-Base.getindex(A::AbstractGeoSeries{<:AbstractGeoStack}, I::Vararg{<:Integer}) = 
-    rebuild(data(A)[I...]; window=window(A), refdims=slicedims(A, I)[2])
+Base.getindex(A::AbstractGeoSeries{<:AbstractGeoStack}, I::Vararg{<:Integer}) =
+    rebuild(data(A)[I...]; refdims=slicedims(A, I)[2])
 
 
 # Concrete implementation ##############################################
@@ -39,7 +39,7 @@ struct GeoSeries{T,N,D,R,A<:AbstractArray{T,N},M,CT,CD,V} <: AbstractGeoSeries{T
     window::V
 end
 
-GeoSeries(data, dims; refdims=(), metadata=Dict(), childtype=GeoStack, childdims=(), window=()) = 
+GeoSeries(data, dims; refdims=(), metadata=Dict(), childtype=GeoStack, childdims=(), window=()) =
     GeoSeries(data, formatdims(data, dims), refdims, metadata, childtype, childdims, window)
 
 @inline rebuild(A::GeoSeries, data, newdims, newrefdims) =
