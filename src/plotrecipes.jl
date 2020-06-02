693b6631441f71778fc3334759f93118ed7a600d
Rafael Schouten <rafaelschouten@gmail.com>
2020-06-02 20:06:25 +1000

    better plots

--- src/plotrecipes.jl
+++ src/plotrecipes.jl
@@ -1,6 +1,23 @@
 using DimensionalData: refdims_title
 
-@recipe function f(A::GeoArray{T,3,<:Tuple{<:Lat,<:Lon,D}}) where {T,D}
+struct GeoPlot end
+
+
+# We only look at arrays with Lat/Lon here.
+# Otherwise they fall back to DimensionalData.jl recipes
+@recipe function f(A::AbstractGeoArray)
+    A = GeoArray(A)
+    if all(hasdim(A, (Lat(), Lon())))
+        GeoPlot(), A
+    else
+        println("DimPlot")
+        da = A |> GeoArray |> a -> DimensionalArray(a; dims=maybe_reproject(dims(a)))
+        DimensionalData.DimensionalPlot(), da
+    end
+end
+
+# Plot 3d arrays as multiple tiled plots
+@recipe function f(::GeoPlot, A::GeoArray{T,3,<:Tuple{<:Lat,<:Lon,D}}) where {T,D}
     A = prepare(A)
     nplots = size(A, 3)
     if nplots > 1
@@ -12,50 +29,46 @@ using DimensionalData: refdims_title
                 aspect_ratio := 1
                 subplot := i
                 slice = A[:, :, i]
-                lat, lon = map(maybe_reproject, dims(A))
-                lon, lat, data(slice)
+                lat, lon = maybe_reproject(dims(A))
+                val(lon), val(lat), data(slice)
             end
         end
     else
-        A[:, :, 1]
+        println("1")
+        GeoPlot(), A[:, :, 1]
     end
 end
 
-# TODO generalise for any dimension order
-@recipe function f(A::GeoArray{T,3}) where {T,D}
-    if all(hasdim(A, (Lat, Lon)))
-        permutedims(A, (Lat, Lon, Dimension))
-    else
-        error("Cannot plot 3 dimensional GeoArray without both Lat/Lon dims")
-    end
+# # Permute for correct Lat/Lon order
+@recipe function f(::GeoPlot, A::GeoArray{T,3}) where {T}
+    GeoPlot(), permutedims(A, (Lat, Lon, Dimension))
 end
 
-@recipe function f(A::GeoArray{T,2,<:Tuple{<:Lat,<:Lon}}) where T
+# # Plot a sinlge 2d map
+@recipe function f(::GeoPlot, A::GeoArray{T,2,<:Tuple{<:Lat,<:Lon}}) where T
     A = prepare(A)
     :seriestype --> :heatmap
     :aspect_ratio --> 1
-    :grid --> false
     :colorbar_title --> name(A)
     :title --> refdims_title(A)
-    lat, lon = map(maybe_reproject, dims(A, (Lat, Lon)))
-    lon, lat, data(A)
+    lat, lon = maybe_reproject(dims(A))
+    val(lon), val(lat), data(A)
 end
 
-@recipe function f(A::GeoArray{T,2,<:Tuple{<:Lon,<:Lat}}) where T
-    permutedims(A)
+# # Permute for correct Lat/Lon order
+@recipe function f(::GeoPlot, A::GeoArray{T,2,<:Tuple{<:Lon,<:Lat}}) where T
+    GeoPlot(), permutedims(A)
 end
 
-@recipe function f(A::AbstractGeoArray)
-    GeoArray(A)
-end
-
+maybe_reproject(dims::Tuple) = map(maybe_reproject, dims)
 maybe_reproject(dim::Dimension) = maybe_reproject(mode(dim), dim)
-maybe_reproject(mode::IndexMode, dim::Dimension) = val(dim)
+maybe_reproject(mode::IndexMode, dim::Dimension) = dim
 maybe_reproject(mode::Projected, dim::Dimension) =
     maybe_reproject(crs(mode), usercrs(mode), dim)
-maybe_reproject(crs, usercrs, dim::Dimension) = val(dim)
-maybe_reproject(crs::GeoFormat, usercrs::GeoFormat, dim::Dimension) =
-    reproject(crs, usercrs, dim, val(dim))
+maybe_reproject(crs, usercrs, dim::Dimension) = dim
+maybe_reproject(crs::GeoFormat, usercrs::GeoFormat, dim::Dimension) = begin
+    rebuild(dim, reproject(crs, usercrs, dim, val(dim)))
+end
 
 prepare(A) = A |> forwardorder |> maybenanmissing
 
