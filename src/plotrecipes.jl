86f032d37d568e3b66664ae49d66921757122a91
Rafael Schouten <rafaelschouten@gmail.com>
2020-06-05 21:57:53 +1000

    tweak plots

--- src/plotrecipes.jl
+++ src/plotrecipes.jl
@@ -8,9 +8,8 @@ struct GeoPlot end
 @recipe function f(A::AbstractGeoArray)
     A = GeoArray(A)
     if all(hasdim(A, (Lat(), Lon())))
-        GeoPlot(), A
+        GeoPlot(), prepare(A)
     else
-        println("DimPlot")
         da = A |> GeoArray |> a -> DimensionalArray(a; dims=maybe_reproject(dims(a)))
         DimensionalData.DimensionalPlot(), da
     end
@@ -18,23 +17,21 @@ end
 
 # Plot 3d arrays as multiple tiled plots
 @recipe function f(::GeoPlot, A::GeoArray{T,3,<:Tuple{<:Lat,<:Lon,D}}) where {T,D}
-    A = prepare(A)
     nplots = size(A, 3)
     if nplots > 1
         :layout --> nplots
-        :xlabel --> permutedims(string.(val(dims(A, D))))
+        :xguide --> permutedims(string.(val(dims(A, D))))
         for i in 1:nplots
             @series begin
                 seriestype := :heatmap
                 aspect_ratio := 1
                 subplot := i
                 slice = A[:, :, i]
-                lat, lon = maybe_reproject(dims(A))
-                val(lon), val(lat), data(slice)
+                lat, lon = map(val, maybe_reproject(dims(A)))
+                lon, lat, parent(slice)
             end
         end
     else
-        println("1")
         GeoPlot(), A[:, :, 1]
     end
 end
@@ -46,13 +43,13 @@ end
 
 # # Plot a sinlge 2d map
 @recipe function f(::GeoPlot, A::GeoArray{T,2,<:Tuple{<:Lat,<:Lon}}) where T
-    A = prepare(A)
     :seriestype --> :heatmap
     :aspect_ratio --> 1
     :colorbar_title --> name(A)
     :title --> refdims_title(A)
-    lat, lon = maybe_reproject(dims(A))
-    val(lon), val(lat), data(A)
+    lat, lon = map(val, maybe_reproject(dims(A)))
+    println(map(typeof, (lon, lat, parent(A))))
+    lon, lat, parent(A)
 end
 
 # # Permute for correct Lat/Lon order
@@ -66,11 +63,10 @@ maybe_reproject(mode::IndexMode, dim::Dimension) = dim
 maybe_reproject(mode::Projected, dim::Dimension) =
     maybe_reproject(crs(mode), usercrs(mode), dim)
 maybe_reproject(crs, usercrs, dim::Dimension) = dim
-maybe_reproject(crs::GeoFormat, usercrs::GeoFormat, dim::Dimension) = begin
+maybe_reproject(crs::GeoFormat, usercrs::GeoFormat, dim::Dimension) =
     rebuild(dim, reproject(crs, usercrs, dim, val(dim)))
-end
 
-prepare(A) = A |> forwardorder |> maybenanmissing
+prepare(A) = A |> maybereplace_missing  |> forwardorder
 
-maybenanmissing(A::AbstractArray{<:AbstractFloat}) = replace_missing(A, missing)
-maybenanmissing(A) = A
+maybereplace_missing(A::AbstractArray{<:AbstractFloat}) = replace_missing(A, NaN)
+maybereplace_missing(A) = A
