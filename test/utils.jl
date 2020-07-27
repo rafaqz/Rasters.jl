642febea38825017169bfcae7b780ae9b48868c5
Rafael Schouten <rafaelschouten@gmail.com>
2020-07-27 22:49:39 +1000

    fix shiftindexloci tests

--- test/utils.jl
+++ test/utils.jl
@@ -8,15 +8,15 @@ end
 
 @testset "shiftindexloci" begin
     dim = X(1.0:3.0; mode=Sampled(Ordered(), Regular(1.0), Intervals(Center())))
-    @test shiftindexloci(dim, Start()) == 0.5:1.0:2.5
-    @test shiftindexloci(dim, End()) == 1.5:1.0:3.5
-    @test shiftindexloci(dim, Center()) == 1.0:1.0:3.0
+    @test shiftindexloci(Start(), dim) == 0.5:1.0:2.5
+    @test shiftindexloci(End(), dim) == 1.5:1.0:3.5
+    @test shiftindexloci(Center(), dim) == 1.0:1.0:3.0
     dim = X([3, 4, 5]; mode=Sampled(Ordered(), Regular(1), Intervals(Start())))
-    @test shiftindexloci(dim, End()) == [4, 5, 6]
-    @test shiftindexloci(dim, Center()) == [3.5, 4.5, 5.5]
-    @test shiftindexloci(dim, Start()) == [3, 4, 5]
+    @test shiftindexloci(End(), dim) == [4, 5, 6]
+    @test shiftindexloci(Center(), dim) == [3.5, 4.5, 5.5]
+    @test shiftindexloci(Start(), dim) == [3, 4, 5]
     dim = X([3, 4, 5]; mode=Sampled(Ordered(), Regular(1), Intervals(End())))
-    @test shiftindexloci(dim, End()) == [3, 4, 5]
-    @test shiftindexloci(dim, Center()) == [2.5, 3.5, 4.5]
-    @test shiftindexloci(dim, Start()) == [2, 3, 4]
+    @test shiftindexloci(End(), dim) == [3, 4, 5]
+    @test shiftindexloci(Center(), dim) == [2.5, 3.5, 4.5]
+    @test shiftindexloci(Start(), dim) == [2, 3, 4]
 end
