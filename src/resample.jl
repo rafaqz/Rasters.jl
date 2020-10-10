770510f60154675e02747f60ed9e48f88a3edca5
Vincent Landau <vincent@csp-inc.org>
2020-10-10 02:36:56 -0600

    first pass at resample function

---
+++ src/resample.jl
@@ -0,0 +1,13 @@
+function resample(A::GeoArray, proj::GeoFormat, resolution::Number;
+                  method::String = "near")
+    wkt = convert(String, convert(WellKnownText, proj))
+
+    AG.Dataset(A) do dataset
+        AG.gdalwarp([dataset], ["-t_srs", "$(wkt)",
+                                      "-te_srs", "$(wkt)",
+                                      "-tr", "$(resolution)", "$(resolution)",
+                                      "-r", "$(method)"]) do warped
+            GeoArray(warped)
+        end
+    end
+end
