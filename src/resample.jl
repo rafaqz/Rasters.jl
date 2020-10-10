29af6d0c443690f9160a88c5e9420c53c6f75574
Vincent Landau <vlandau@users.noreply.github.com>
2020-10-10 12:41:10 -0600

    Remove unnecessary line with target extent srs

--- src/resample.jl
+++ src/resample.jl
@@ -4,7 +4,6 @@ function resample(A::GeoArray, proj::GeoFormat, resolution::Number;
 
     AG.Dataset(A) do dataset
         AG.gdalwarp([dataset], ["-t_srs", "$(wkt)",
-                                      "-te_srs", "$(wkt)",
                                       "-tr", "$(resolution)", "$(resolution)",
                                       "-r", "$(method)"]) do warped
             GeoArray(warped)
