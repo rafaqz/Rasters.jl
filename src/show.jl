015d8f44fce6e84e74479fd1e3d3e7ed8c389f96
Rafael Schouten <rafaelschouten@gmail.com>
2020-11-06 22:28:52 +1100

    actually add the show.jl file

---
+++ src/show.jl
@@ -0,0 +1,23 @@
+
+# Don't show the data in DiskGeoArray. It 
+# defeats the purpose of loading them lazily.
+Base.show(io::IO, A::DiskGeoArray) = begin
+    l = nameof(typeof(A))
+    printstyled(io, nameof(typeof(A)); color=:blue)
+    if label(A) != ""
+        print(io, " (named ")
+        printstyled(io, label(A); color=:blue)
+        print(io, ")")
+    end
+    print(io, " with dimensions:\n")
+    for d in dims(A)
+        print(io, " ", d, "\n")
+    end
+    if !isempty(refdims(A))
+        print(io, "and referenced dimensions:\n")
+        for d in refdims(A)
+            print(io, " ", d, "\n")
+        end
+    end
+    print(io, "\n  From file: $(filename(A))")
+end
