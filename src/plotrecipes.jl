3b8ca9cfc568ba0b626bb0afe96d04c51b55fa68
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-11-24 20:35:05 -0500

    Dispatch plot recipes on Geo{X,Y}Dim

--- src/plotrecipes.jl
+++ src/plotrecipes.jl
@@ -1,21 +1,21 @@
 struct GeoPlot end
 
-# We only look at arrays with Lat/Lon here.
+# We only look at arrays with <:GeoXDim/<:GeoYDim here.
 # Otherwise they fall back to DimensionalData.jl recipes
 @recipe function f(A::AbstractGeoArray)
     A = GeoArray(A)
-    if all(hasdim(A, (Lat(), Lon())))
+    if all(hasdim(A, (GeoXDim, GeoYDim)))
         # Heatmap or multiple heatmaps. Use GD recipes.
         GeoPlot(), prepare(A)
     else
-        # This is not a Lat/Lon heatmap. Fall back to DD recipes after reprojecting
+        # This is not a GeoXDim/GeoYDim heatmap. Fall back to DD recipes after reprojecting
         da = A |> GeoArray |> a -> DimArray(a; dims=_maybe_mapped(dims(a)))
         DimensionalData.DimensionalPlot(), da
     end
 end
 
 # Plot 3d arrays as multiple tiled plots
-@recipe function f(::GeoPlot, A::GeoArray{T,3,<:Tuple{<:Lat,<:Lon,D}}) where {T,D}
+@recipe function f(::GeoPlot, A::GeoArray{T,3,<:Tuple{<:GeoXDim,<:GeoYDim,D}}) where {T,D}
     nplots = size(A, 3)
     if nplots > 1
         :layout --> nplots
@@ -35,13 +35,13 @@ end
     end
 end
 
-# # Permute for correct Lat/Lon order
-@recipe function f(::GeoPlot, A::GeoArray{T,3}) where {T}
-    GeoPlot(), permutedims(A, (Lat, Lon, Dimension))
+# # Permute for correct order
+@recipe function f(::GeoPlot, A::GeoArray{T,3}) where T
+    GeoPlot(), permutedims(A, (GeoXDim, GeoYDim, Dimension))
 end
 
 # # Plot a sinlge 2d map
-@recipe function f(::GeoPlot, A::GeoArray{T,2,<:Tuple{<:Lat,<:Lon}}) where T
+@recipe function f(::GeoPlot, A::GeoArray{T,2,<:Tuple{<:GeoXDim,<:GeoYDim}}) where T
     :seriestype --> :heatmap
     :aspect_ratio --> 1
     :colorbar_title --> name(A)
@@ -50,8 +50,8 @@ end
     lons, lats, parent(A)
 end
 
-# # Permute for correct Lat/Lon order
-@recipe function f(::GeoPlot, A::GeoArray{T,2,<:Tuple{<:Lon,<:Lat}}) where T
+# # Permute for correct order
+@recipe function f(::GeoPlot, A::GeoArray{T,2,<:Tuple{<:GeoXDim,<:GeoYDim}}) where T
     GeoPlot(), permutedims(A)
 end
 
