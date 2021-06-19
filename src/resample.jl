af4a7d7e58d76fcb52fed8fe60978f301a5dda28
Rafael Schouten <rafaelschouten@gmail.com>
2021-06-19 18:08:05 +1000

    resample args follow other methods

--- src/resample.jl
+++ src/resample.jl
@@ -1,9 +1,8 @@
 export resample
 
 """
-	resample(A::AbstractGeoArray, resolution::Number;
-			 crs::GeoFormat=crs(A), method::String="near")
-	resample(A::AbstractGeoArray, snap::AbstractGeoArray; method::String="near")
+	resample(A::AbstractGeoArray, resolution::Number; crs, method)
+	resample(A::AbstractGeoArray; to::AbstractGeoArray, method)
 
 `resample` uses `ArchGDAL.gdalwarp` to resample an `AbstractGeoArray`.
 
@@ -12,22 +11,21 @@ export resample
 - `A`: The `AbstractGeoArray` to resample.
 - `resolution`: A `Number` specifying the resolution for the output.
     If the keyword argument `crs` (described below) is specified, `resolution` must be in units of the `crs`.
-- `snap`: an `AbstractGeoArray` whos resolution, crs and bounds will be snapped to.
-    For best results it should roughly cover the same extent, or a subset of `A`.
 
 # Keywords
 
+- `to`: an `AbstractGeoArray` whos resolution, crs and bounds will be snapped to.
+    For best results it should roughly cover the same extent, or a subset of `A`.
 - `crs`: A `GeoFormatTypes.GeoFormat` specifying an output crs
-    (`A` with be reprojected to `crs` in addition to being resampled). Defaults to `crs(A)`
-- `method`: A `String` specifying the method to use for resampling. Defaults to `"near"`
+    (`A` will be reprojected to `crs` in addition to being resampled). Defaults to `crs(A)`
+- `method`: A `Symbol` or `String` specifying the method to use for resampling. Defaults to `:near`
     (nearest neighbor resampling). See [resampling method](https://gdal.org/programs/gdalwarp.html#cmdoption-gdalwarp-r)
     in the gdalwarp docs for a complete list of possible values.
 
 """
 function resample end
 function resample(A::AbstractGeoArray, resolution::Number;
-    crs::GeoFormat=crs(A),
-    method::String="near"
+    crs::GeoFormat=crs(A), method=:near
 )
     wkt = convert(String, convert(WellKnownText, crs))
     flags = ["-t_srs", "$(wkt)",
@@ -39,14 +37,14 @@ function resample(A::AbstractGeoArray, resolution::Number;
         end
     end
 end
-function resample(A::AbstractGeoArray, snap::AbstractGeoArray; method::String="near")
-    wkt = convert(String, convert(WellKnownText, crs(snap)))
-    latres, lonres = map(abs ∘ step, span(snap, (Y(), X())))
-    (latmin, latmax), (lonmin, lonmax) = bounds(snap, (Y(), X()))
+function resample(A::AbstractGeoArray; to, method=:near)
+    wkt = convert(String, convert(WellKnownText, crs(to)))
+    latres, lonres = map(abs ∘ step, span(to, (Y(), X())))
+    (latmin, latmax), (lonmin, lonmax) = bounds(to, (Y(), X()))
     flags = ["-t_srs", "$(wkt)",
              "-tr", "$(latres)", "$(lonres)",
              "-te", "$(lonmin)", "$(latmin)", "$(lonmax)", "$(latmax)",
-             "-r", "$(method)"]
+             "-r", "$(string(method))"]
     AG.Dataset(A) do dataset
         AG.gdalwarp([dataset], flags) do warped
             read(GeoArray(warped))
