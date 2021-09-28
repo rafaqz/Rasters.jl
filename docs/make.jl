845dea3b9c6ea05185d1ed7bcb4a997465581ebb
rafaqz <rafaelschouten@gmail.com>
2021-09-28 10:02:31 +0200

    not strict

--- docs/make.jl
+++ docs/make.jl
@@ -3,17 +3,17 @@ using Documenter, GeoData, Plots, Logging
 ENV["GKSwstype"] = "100"
 
 # Disable logging as @info messes with doctests
-# Logging.disable_logging(Logging.Warn)
+Logging.disable_logging(Logging.Warn)
 
 # Make the docs, without running the tests again
 makedocs(
     modules = [GeoData],
     sitename = "GeoData.jl",
-    strict = true,
+    strict = false,
 )
 
 # Enable logging to console again
-# Logging.disable_logging(Logging.BelowMinLevel)
+Logging.disable_logging(Logging.BelowMinLevel)
 
 deploydocs(
     repo = "github.com/rafaqz/GeoData.jl.git",
