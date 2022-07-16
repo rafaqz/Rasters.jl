4af0be7900c774b742a522ac4e2c1e862cf8b065
rafaqz <rafaelschouten@gmail.com>
2022-07-16 02:06:51 +0200

    add zonal stats

--- src/methods/zonal.jl
+++ src/methods/zonal.jl
@@ -1,13 +1,16 @@
 """
-    zonal(f, x::RasterStackOrArray, obj)
+    zonal(f, x::RasterStackOrArray, zone)
 
-Calculate zonal statistics for an object or table.
+Calculate zonal statistics for the the zone of a `Raster` or `RasterStack` defined
+by the object `zone`.
 
 # Arguments
 
 - `f`: aby function that reduces an iterable to a single value, such as `Base.sum` or `Statistic.mean`
 - `x`: A `Raster` or `RasterStack`
-- `obj`: A `Raster`, `RasterStack`, dim tuple, extent or GeoInterface.jl compatible geometry.
+- `zone`: A `Raster`, `RasterStack`, dim tuple, extent, GeoInterface.jl compatible geometry,
+    Tables.jl compatible table with a `:geometry` column, or an `AbstractVector` of
+    any of these objects..
 
 # Example
 
@@ -22,7 +25,7 @@ isfile(shapefile_name) || Downloads.download(shp_url, shp_name)
 isfile(dbf_url) || Downloads.download(dbf_url, dbf_name)
 
 # Download and read a raster stack
-st = read(RasterStack(WorldClim{Climate}; month=Jan))
+st = RasterStack(WorldClim{Climate}; month=Jan, lazy=false)
 
 # Load the shapes for world countries
 countries = Shapefile.Table(shapefile_name) |> DataFrame
