c4d0f78d5fe9c89eb2b6f50466505a18cfa0bc36
Josquin Guerber <josquin.guerber@gmail.com>
2022-08-28 13:49:11 +0200

    basic read

--- src/sources/ascii.jl
+++ src/sources/ascii.jl
@@ -1,3 +1,5 @@
+const ASCII_DEFAULT_CRS = EPSG(4326)
+
 has_layers(::Type{ASCIIfile}) = false
 
 struct ASCIIparams{T, F}
@@ -17,4 +19,103 @@ filename(ap::ASCIIparams) = ap.filename
 
 # DimensionalData methods
 
-# Base methods
+function DD.dims(ap::ASCIIparams, crs=nothing, mappedcrs=nothing)
+    crs = crs isa Nothing ? ASCII_DEFAULT_CRS : crs
+
+    nc, nr = size(ap)
+
+    pars = params(ap)
+
+    xbounds = (pars[:xll], pars[:xll] + pars[:dx] * nc)
+    ybounds = (pars[:yll], pars[:yll] + pars[:dy] * nr)
+
+    # Always intervals
+    xspan = (xbounds[2] - xbounds[1]) / nc
+    yspan = (ybounds[2] - ybounds[1]) / nr
+
+    # Not fully implemented yet
+    xy_metadata = Metadata{ASCIIfile}(Dict())
+
+    xindex = LinRange(xbounds[1], xbounds[2] - xspan, nc)
+    yindex = LinRange(ybounds[2] - yspan, ybounds[1], nr)
+
+    xlookup = Projected(xindex;
+        order=ForwardOrdered(),
+        span=Regular(xspan),
+        sampling=Intervals(Start()),
+        metadata=xy_metadata,
+        crs=crs,
+        mappedcrs=mappedcrs,
+        dim=X()
+    )
+    ylookup = Projected(yindex;
+        order= ReverseOrdered(),
+        span=Regular(yspan),
+        sampling=Intervals(Start()),
+        metadata=xy_metadata,
+        crs=crs,
+        mappedcrs=mappedcrs,
+        dim=Y()
+    )
+
+    x = X(xlookup)
+    y = Y(ylookup)
+
+    return x,y
+
+end
+
+missingval(ap::ASCIIparams) = params(ap)[:nodatavalue]
+
+DD.metadata(ap::ASCIIparams) = Metadata{ASCIIfile}()
+DD.ndims(ap::ASCIIparams) = 2
+
+# Base util methods
+
+Base.eltype(p::ASCIIparams{T}) where T = T
+
+function Base.size(ap::ASCIIparams)
+    params(ap)[:ncols], params(ap)[:nrows]
+end
+
+Base.Array(ap::ASCIIparams) = _open(a -> Array(a), ap)
+
+# Array
+function FileArray(ap::ASCIIparams, filename = filename(ap); kw...)
+    size_ = size(ap)
+    eachchunk = DiskArrays.GridChunks(size_, size_)
+    haschunks = DiskArrays.Unchunked()
+    T = eltype(ap)
+    N = ndims(ap)
+    FileArray{ASCIIfile, T, N}(filename, size_; eachchunk, haschunks, kw...)
+end
+
+# Base i/o methods
+
+function Base.open(f::Function, A::FileArray{ASCIIfile}, key...; write = A.write)
+    _open(dat -> f(RasterDiskArray{ASCIIfile}(dat, A.eachchunk, A.haschunks)), A; write)
+end
+
+# also called by _open(f, fa::FileArray{ASCIIfile})
+function _open(f, ::Type{ASCIIfile}, filename; write = false, kw...)
+    isfile(filename) || _filenotfound_error(filename)
+    _open(f, ASCIIfile, ASCIIparams(filename; write))
+end
+
+_open(f, ::Type{ASCIIfile}, ap::ASCIIparams; kw...) = f(ap)
+
+function _open(f::Function, ap::ASCIIparams; kw...)
+    _asciigrid(f, filename(ap), eltype(ap), size(ap); kw...)
+end
+
+function _asciigrid(f, filename::AbstractString, T::Type, size::Tuple; write = false, kw...)
+    out = open(filename, "r") do io
+        dat, pars = ASCIIrasters.read_ascii(filename; lazy = false)
+        output = f(dat)
+        output
+    end
+    if write
+        ASCIIrasters.write_ascii(filename, dat; pars...)
+    end
+    out
+end
