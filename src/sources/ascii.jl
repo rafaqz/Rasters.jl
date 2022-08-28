963059f2452a95c54c375e1c1fa620821f833353
Josquin Guerber <josquin.guerber@gmail.com>
2022-08-28 11:37:07 +0200

    ASCIIparams type and accessors

--- src/sources/ascii.jl
+++ src/sources/ascii.jl
@@ -1,1 +1,20 @@
 has_layers(::Type{ASCIIfile}) = false
+
+struct ASCIIparams{T, F}
+    filename::F
+    params::NamedTuple
+    write::Bool
+end
+
+function ASCIIparams(filename::AbstractString; write=false)
+    pars = ASCIIrasters.read_ascii(filename; lazy = true)
+    T = typeof(pars[:nodatavalue])
+    ASCIIparams{T, typeof(filename)}(filename, pars, write)
+end
+
+params(ap::ASCIIparams) = ap.params
+filename(ap::ASCIIparams) = ap.filename
+
+# DimensionalData methods
+
+# Base methods
