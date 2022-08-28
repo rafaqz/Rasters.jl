b67ef616d44fa3a8d3a7fe1294374416eb85b327
Josquin Guerber <josquin.guerber@gmail.com>
2022-08-28 14:57:05 +0200

    some working tests

--- test/sources/ascii.jl
+++ test/sources/ascii.jl
@@ -1,4 +1,5 @@
 using Rasters, Test, Dates, DimensionalData
+using Rasters.LookupArrays, Rasters.Dimensions
 using Rasters: FileArray, ASCIIfile
 
 const DD = DimensionalData
@@ -41,6 +42,40 @@ ascpath = "britanny.asc"
         
     @testset "array properties" begin
         @test ascarray isa Raster{Float64,2}
+    end
+
+    @testset "dimensions" begin
+        @test length(val(dims(dims(ascarray), X))) == 401
+        @test ndims(ascarray) == 2
+        @test dims(ascarray) isa Tuple{<:X,<:Y}
+        @test refdims(ascarray) == ()
+        @test bounds(ascarray) == ((-4.591421949457, -3.3483663541919997), (47.832347916354, 48.665462015892))
+    end
+
+    @testset "fields" begin
+        @test missingval(ascarray) == -9999.0
+        @test metadata(ascarray) isa Metadata{ASCIIfile}
+        @test name(ascarray) == Symbol("")
+        @test label(ascarray) == ""
+        @test units(ascarray) === nothing
+        custom = Raster(ascpath; name = :ascii, mappedcrs = EPSG(4326))
+        @test name(custom) == :ascii
+        @test label(custom) == "ascii"
+        @test mappedcrs(dims(custom, Y)) == EPSG(4326)
+        @test mappedcrs(dims(custom, X)) == EPSG(4326)
+        @test mappedcrs(custom) == EPSG(4326)
+        @test crs(dims(custom, Y)) == EPSG(4326)
+        @test crs(dims(custom, X)) == EPSG(4326)
+        @test crs(custom) == EPSG(4326)
+    end
+
+    @testset "getindex" begin
+        @test ascarray[X(1)] isa Raster{Float64, 1}
+        @test ascarray[Y(1)] isa Raster{Float64, 1}
+        @test ascarray[X(12), Y(56)] == 5340.0
+        @test ascarray[35, 42] == 7482.0
+        @test ascarray[Y(At(20.0; atol=1e10)), X(At(20; atol=1e10))] == 5500.0
+        @test ascarray[Y(Contains(48.5)), X(Contains(-4.2))] == 6624.0
     end
 
 end
