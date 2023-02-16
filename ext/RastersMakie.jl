82f723c7f6f9e22dd37093c6f3ea4d9fa9d6136d
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-02-16 10:11:35 +0530

    add a ColorTypes extension to the extension package

--- ext/RastersMakie.jl
+++ ext/RastersMakie.jl
@@ -14,6 +14,17 @@ end
 
 using Rasters.DimensionalData
 
+# first, some Makie utils which require Makie types
+MakieCore.plottype(::AbstractRaster{<: Makie.Colors.Colorant, 2}) = MakieCore.Image
+function MakieCore.convert_arguments(::MakieCore.SurfaceLike, raw_raster::AbstractRaster{<: Makie.Colors.Colorant, 2})
+    ds = DD._fwdorderdims(raw_raster)
+    A = permutedims(raw_raster, ds)
+    x, y = dims(A)
+    xs, ys, zs = DD._withaxes(x, y, (A))
+    return (xs, ys, collect(zs))
+end
+
+# now, the "full" plot-func
 
 """
     _balance_grid(nplots)
