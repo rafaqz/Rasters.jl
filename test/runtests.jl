aeddac69beb437bd69f7decfc91fd896b06832d6
rafaqz <rafaelschouten@gmail.com>
2023-05-11 10:55:21 +0200

    run plots last

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,7 +20,6 @@ end
 @time @safetestset "rasterize" begin include("rasterize.jl") end
 @time @safetestset "methods" begin include("methods.jl") end
 @time @safetestset "adapt" begin include("adapt.jl") end
-@time @safetestset "plot recipes" begin include("plotrecipes.jl") end
 # Only test SMAP locally for now, also RasterDataSources because CI dowloads keep breaking
 if !haskey(ENV, "CI")
     @time @safetestset "smap" begin include("sources/smap.jl") end
@@ -32,3 +31,4 @@ if !Sys.iswindows()
     @time @safetestset "grd" begin include("sources/grd.jl") end
 end
 @time @safetestset "ncdatasets" begin include("sources/ncdatasets.jl") end
+@time @safetestset "plot recipes" begin include("plotrecipes.jl") end
