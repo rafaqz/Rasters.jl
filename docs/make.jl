97031a4e81f62dd4c38cc89d131c32bd180ab0a3
rafaqz <rafaelschouten@gmail.com>
2023-10-30 12:31:49 +0100

    no strict warning in mkdocs

--- docs/make.jl
+++ docs/make.jl
@@ -40,18 +40,15 @@ makedocs(
     authors="Rafael Schouten et al.",
     clean=true,
     doctest=true,
-    strict=false,
-    # [
-        # :doctest,
-        # :linkcheck,
-        # :parse_error,
-        # :example_block,
+    strict=[
+        :doctest,
+        :linkcheck,
+        :parse_error,
+        :example_block,
         # Other available options are
         # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block,
         # :footnote, :meta_block, :missing_docs, :setup_block
-    # ], 
-    # checkdocs=:all, 
-    format=Markdown(), draft=false,
+    ], checkdocs=:all, format=Markdown(), draft=false,
     build=joinpath(@__DIR__, "docs")
 )
 
