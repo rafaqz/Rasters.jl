50083279e6e25d9b24ffee70fc2012f3a77ebc13
rafaqz <rafaelschouten@gmail.com>
2023-11-15 14:34:29 +0100

    document, test and add keyword method

--- ext/RastersArchGDALExt/reproject.jl
+++ ext/RastersArchGDALExt/reproject.jl
@@ -4,6 +4,7 @@ function Rasters._reproject(source::GeoFormat, target::GeoFormat, ::XDim, vals::
     push!(paired_vals, (first(vals), one(first(vals))))
     reps = AG.reproject(paired_vals, source, target; order=:trad)
     at_one = pop!(reps)
+    # TODO is this test sufficient to reject the reprojection?
     first(reps)[1] == at_one[1] || _reproject_crs_error(source, target)
     return map(r -> r[1], reps)
 end
@@ -16,4 +17,5 @@ function Rasters._reproject(source::GeoFormat, target::GeoFormat, dim::YDim, val
     return map(r -> r[2], reps)
 end
 
-_reproject_crs_error(source, target) = throw(ArgumentError("Cannot reproject from: \n $source \nto: \n $target")) 
+_reproject_crs_error(source, target) = 
+    throw(ArgumentError("Cannot reproject from: \n $source \nto: \n $target, coordinate reference systems are not aligned with on all axes. You may need to use `resample` instead")) 
