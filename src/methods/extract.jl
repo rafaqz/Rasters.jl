ebc28b0462de7b38e1a0697c8f8594a18871efc8
rafaqz <rafaelschouten@gmail.com>
2023-11-24 21:42:40 +0100

    attribution
    Co-authored-by: Tiem van der Deure <tiemvanderdeure@gmail.com>

--- src/methods/extract.jl
+++ src/methods/extract.jl
@@ -16,7 +16,7 @@ sliced arrays or stacks will be returned instead of single values.
 # Keywords
 
 - `geometry`: include a `:geometry` column with the corresponding points for each value, `true` by default.
-- `index`: include a column of the `CartesianIndex` for each value, `false` by default.
+- `index`: include an `:index` column of the `CartesianIndex` for each value, `false` by default.
 - `names`: `Tuple` of `Symbol` corresponding to layers of a `RasterStack`. All layers by default.
 - `skipmissing`: skip missing points automatically.
 - `atol`: a tolorerance for floating point lookup values for when the `LookupArray`
