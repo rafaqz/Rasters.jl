3a74f36d27425a4739f55d3f294b3e2e918cff38
Tiem van der Deure <tiemvanderdeure@gmail.com>
2024-04-03 17:31:13 +0200

    fix reading a lazy rasterstack (#621)
    * fix _open_layers
    
    * add test for reading a lazy rasterstack
    
    * filename(stack) maps over layers
    
    * fix type in test

--- src/stack.jl
+++ src/stack.jl
@@ -29,7 +29,7 @@ subset without loading the whole array.
 abstract type AbstractRasterStack{L} <: AbstractDimStack{L} end
 
 missingval(stack::AbstractRasterStack) = getfield(stack, :missingval)
-filename(stack::AbstractRasterStack) = filename(parent(stack))
+filename(stack::AbstractRasterStack) = map(s -> filename(s), stack)
 missingval(s::AbstractRasterStack, key::Symbol) = _singlemissingval(missingval(s), key)
 
 isdisk(st::AbstractRasterStack) = isdisk(layers(st, 1))
@@ -434,13 +434,13 @@ function Base.open(f::Function, st::AbstractRasterStack{<:NamedTuple}; kw...)
 end
 
 # Open all layers through nested closures, applying `f` to the rebuilt open stack
-_open_layers(f, st) = _open_layers(f, st, DD.layers(f), NamedTuple())
+_open_layers(f, st) = _open_layers(f, st, DD.layers(st), NamedTuple())
 function _open_layers(f, st, unopened::NamedTuple{K}, opened::NamedTuple) where K
     open(first(unopened)) do open_layer
-        _open_layers(f, st, Base.tail(unopened), merge(opened, NamedTuple{(first(K))}(open_layer)))
+        _open_layers(f, st, Base.tail(unopened), merge(opened, NamedTuple{(first(K),)}((open_layer,))))
     end
 end
-function _open_layers(f, st, unopened::NamedTuple{()}, opened)
+function _open_layers(f, st, unopened::NamedTuple{()}, opened::NamedTuple)
     f(rebuild(st; data=opened))
 end
 
