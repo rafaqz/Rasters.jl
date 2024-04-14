b97322c4694aa459ada5bd32117bc8e2ab9138b1
rafaqz <rafaelschouten@gmail.com>
2024-04-14 10:00:15 +0200

    fix chunk tests

--- test/utils.jl
+++ test/utils.jl
@@ -10,18 +10,23 @@ end
 @testset "_chunks_to_tuple" begin
     A = zeros(100, 100)
     template = DiskArrays.RechunkedDiskArray(A, DiskArrays.GridChunks(A, (32, 16, 1)))
+    # Bool chunk selection is generated from template
     _chunks_to_tuple(template, (X(), Y(), Ti()), true) == (32, 16, 1)
     @test _chunks_to_tuple(template, (X(), Y()), true) == (32, 16)
     @test _chunks_to_tuple(template, (Y(), X()), true) == (32, 16)
     @test _chunks_to_tuple(template, (Y(), X()), false) == nothing
 
+    # Extra dims error
     @test_throws ArgumentError _chunks_to_tuple(template, (Y(), X()), (16, 32, 1))
-    @test_throws ArgumentError _chunks_to_tuple(template, (Y(), X()), (16,))
+    # Missing dims filled with 1s
+    @test _chunks_to_tuple(template, (Y(), X()), (16,)) == (16, 1)
+    # Correct number works as is
     @test _chunks_to_tuple(template, (Y(), X()), (16, 32)) == (16, 32)
 
+    # Named chunks
     @test _chunks_to_tuple(template, (X(), Y()), (X=16, Y=32)) == (16, 32)
     @test _chunks_to_tuple(template, (X(), Y()), (Y=16, X=32)) == (32, 16)
-
+    
     @test _chunks_to_tuple(template, (X(), Y()), (Y(8), X(12))) == (12, 8)
     @test _chunks_to_tuple(template, (X(), Y()), (Y(8), X(12), Ti(5))) == (12, 8)
     @test _chunks_to_tuple(template, (X(), Y(), Ti()), (Y(8), X(12))) == (12, 8, 1)
