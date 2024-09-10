7da0620ef43f61d0889f5f1c30c76b639d81300c
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-09-10 16:11:11 -0700

    Hook up scale and offset to Zarr datasets via CDM
    This fixes a strange issue I was having where Rasters.jl read the wrong values from a Kerchunk/Zarr dataset.

--- ext/RastersZarrDatasetsExt/zarrdatasets_source.jl
+++ ext/RastersZarrDatasetsExt/zarrdatasets_source.jl
@@ -16,6 +16,11 @@ RA._sourcetrait(::ZD.ZarrDataset) = Zarrsource()
 RA.missingval(var::ZD.ZarrVariable, args...) = RA.missingval(RA.Metadata{Zarrsource}(CDM.attribs(var)))
 RA.missingval(var::ZD.ZarrVariable, md::RA.Metadata{<:Zarrsource}) = RA.missingval(md)
 
+@inline function RA.get_scale(metadata::RA.Metadata{<: Zarrsource}, scaled::Bool)
+    scale = scaled ? get(metadata, "scale_factor", nothing) : nothing
+    offset = scaled ? get(metadata, "add_offset", nothing) : nothing
+    return scale, offset
+end
 # TODO: handle multiple missing values
 function RA.missingval(md::RA.Metadata{<:Zarrsource})
     fv = get(md, "_FillValue", nothing)
