8b8fde805822330f7e32593060abcddd7d1e5851
tiemvanderdeure <tiemvanderdeure@gmail.com>
2024-09-27 12:19:31 +0200

    add a test with a reverse order dimension

--- test/cellsize.jl
+++ test/cellsize.jl
@@ -6,6 +6,7 @@ include(joinpath(dirname(pathof(Rasters)), "../test/test_utils.jl"))
 @testset "cellsize" begin
     x = X(Projected(90.0:0.1:99.9; sampling=Intervals(Start()), order = ForwardOrdered(), span = Regular(0.1), crs=EPSG(4326)))
     y = Y(Projected(0.0:0.1:89.9; sampling=Intervals(Start()), order = ForwardOrdered(), span = Regular(0.1), crs=EPSG(4326)))
+    y_rev = Y(Projected(89.9:-0.1:0; sampling=Intervals(Start()), order = ReverseOrdered(), span = Regular(-0.1), crs=EPSG(4326)))
     dimz = (x,y)
 
     dimz_25832 = X(Projected(0.0:100:10000.0; sampling=Intervals(Start()), order = ForwardOrdered(), span = Regular(100), crs=EPSG(25832))),
@@ -14,11 +15,13 @@ include(joinpath(dirname(pathof(Rasters)), "../test/test_utils.jl"))
 
     cs = cellsize(dimz)
     cs2 = cellsize(dimz_25832)
+    cs3 = cellsize((x, y_rev))
 
     # Check the output is a raster 
     @test cs isa Raster
     @test cs2 isa Raster
     # Test that the total area matches the expected area (1/72th of the Earth surface)
+    @test sum(cs) == sum(cs3)
     @test sum(cs) ≈ 510.1e6/72 rtol = 0.01
     # Test all areas are about 0.01 km2
     @test maximum(cs2) ≈ 0.01 rtol = 0.01
