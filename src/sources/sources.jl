b1fe1bf64197d01c89ada33254b8c991120c11c9
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-10-02 16:10:06 -0700

    Mention that packages need to be loaded *manually*
    Co-authored-by: Rafael Schouten <rafaelschouten@gmail.com>

--- src/sources/sources.jl
+++ src/sources/sources.jl
@@ -59,7 +59,7 @@ end
 # error message to show when backend is not loaded
 function Base.showerror(io::IO, e::BackendException)
     printstyled(io, "Rasters.jl"; underline = true)
-    printstyled(io, " requires backends to be loaded externally as of version 0.8.  Run ")
+    printstyled(io, " requires backends to be loaded manually.  Run ")
     printstyled(io, "`import $(e.backend)`"; bold = true)
     print(io, "to fix this error.")
 end
