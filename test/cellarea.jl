4906076899da411a8f202eb6f9a3d32c4fef6c0a
tiemvanderdeure <tiemvanderdeure@gmail.com>
2024-10-03 17:43:44 +0200

    test on a 5x5m grid

--- test/cellarea.jl
+++ test/cellarea.jl
@@ -8,14 +8,20 @@ include(joinpath(dirname(pathof(Rasters)), "../test/test_utils.jl"))
     y = Y(Projected(0.0:0.1:89.9; sampling=Intervals(Start()), order = ForwardOrdered(), span = Regular(0.1), crs=EPSG(4326), dim = Y()))
     y_rev = Y(Projected(89.9:-0.1:0; sampling=Intervals(Start()), order = ReverseOrdered(), span = Regular(-0.1), crs=EPSG(4326), dim = Y()))
     dimz = (x,y)
+    ras = ones(dimz)
 
     dimz_25832 = X(Projected(0.0:100:10000.0; sampling=Intervals(Start()), order = ForwardOrdered(), span = Regular(100), crs=EPSG(25832))),
        Y(Projected(0.0:100:10000.0; sampling=Intervals(Start()), order = ForwardOrdered(), span = Regular(100), crs=EPSG(25832)))
-    ras = ones(dimz)
+    # to test that this is still stable on as 5x5m grid
+    smallspan_25832 = X(Projected(0.0:5:100.0; sampling=Intervals(Start()), order = ForwardOrdered(), span = Regular(5), crs=EPSG(25832))),
+       Y(Projected(0.0:5:100.0; sampling=Intervals(Start()), order = ForwardOrdered(), span = Regular(5), crs=EPSG(25832)))
+
+    @btime cs2 = cellarea($dimz_25832)
 
     cs = cellarea(dimz)
     cs2 = cellarea(dimz_25832)
     cs3 = cellarea((x, y_rev))
+    cs4 = cellarea(smallspan_25832)
 
     # check cellsize still works
     csold = @test_warn "deprecated" cellsize(dimz)
@@ -27,9 +33,12 @@ include(joinpath(dirname(pathof(Rasters)), "../test/test_utils.jl"))
     # Test that the total area matches the expected area (1/72th of the Earth surface)
     @test sum(cs) ≈ sum(cs3)
     @test sum(cs) ≈ 510.1e12/72 rtol = 0.01
-    # Test all areas are about 10,000 m2
+    # Test all areas are about 10,000 m2 for the 100x100m grid
     @test maximum(cs2) ≈ 1e4 rtol = 0.01
     @test minimum(cs2) ≈ 1e4 rtol = 0.01
+    # Test all areas are about 25 m2 on the 5x5m grid
+    @test maximum(cs4) ≈ 25 rtol = 0.01
+    @test minimum(cs4) ≈ 25 rtol = 0.01
     # test passing in a raster or dims gives the same result
     cs_ras = cellarea(ras)
     @test cs == cs_ras
