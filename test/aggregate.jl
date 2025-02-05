3a4f09a3814225da02a16951fd85d8e2d2cb77e5
tiemvanderdeure <tiemvanderdeure@gmail.com>
2025-02-05 17:50:38 +0100

    add a test

--- test/aggregate.jl
+++ test/aggregate.jl
@@ -217,3 +217,9 @@ end
     @test_throws ArgumentError aggregate(sum, rast[X=1, Y=1], 2)
 end
 
+@test "Lazy disaggregation" begin
+    eager_disag = disaggregate(array1, (X(2), Y(2))) 
+    lazy_disag = disaggregate(array1, (X(2), Y(2)); view = true)
+    @test eager_disag == lazy_disag
+    @test parent(lazy_disag) isa SubArray
+end
