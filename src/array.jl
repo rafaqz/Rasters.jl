15a3cc228b6b3901948798288ff1ba0736249463
tiemvanderdeure <tiemvanderdeure@gmail.com>
2025-02-06 10:28:06 +0100

    drop the == dispatch on Rasters

--- src/array.jl
+++ src/array.jl
@@ -66,9 +66,6 @@ isdisk(A::AbstractRaster) = parent(A) isa DiskArrays.AbstractDiskArray
 isdisk(x) = false
 ismem(A::AbstractRaster) = !isdisk(A)
 
-function Base.:(==)(A::AbstractRaster{T,N}, B::AbstractRaster{T,N}) where {T,N}
-    size(A) == size(B) && all(A .== B)
-end
 for f in (:mappedbounds, :projectedbounds, :mappedindex, :projectedindex)
     @eval ($f)(A::AbstractRaster, dims_) = ($f)(dims(A, dims_))
     @eval ($f)(A::AbstractRaster) = ($f)(dims(A))
