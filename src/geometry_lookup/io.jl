9db69a16e6dac865aa53f708af5f721cf806eba4
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-04-22 18:35:07 -0400

    use traittarget in io

--- src/geometry_lookup/io.jl
+++ src/geometry_lookup/io.jl
@@ -1,17 +1,17 @@
 
 
 
-function _geometry_cf_encode(::Union{GI.LineStringTrait, GI.MultiLineStringTrait}, geoms)
+function _geometry_cf_encode(::GeometryOpsCore.TraitTarget{<: Union{<: GI.LineStringTrait, <: GI.MultiLineStringTrait}}, geoms)
     error("Not implemented yet")
 end
 
-function _geometry_cf_encode(::Union{GI.PointTrait, GI.MultiPointTrait}, geoms)
+function _geometry_cf_encode(::GeometryOpsCore.TraitTarget{<: Union{<: GI.PointTrait, <: GI.MultiPointTrait}}, geoms)
     error("Not implemented yet")
 end
 
 
 
-function _geometry_cf_encode(::Union{GI.PolygonTrait, GI.MultiPolygonTrait}, geoms)
+function _geometry_cf_encode(::GeometryOpsCore.TraitTarget{<: Union{<: GI.PolygonTrait, <: GI.MultiPolygonTrait}}, geoms)
     n_points_per_geom_vec = GI.npoint.(geoms)
     total_n_points = sum(n_points_per_geom_vec)
 
@@ -67,8 +67,19 @@ function _geometry_cf_encode(::Union{GI.PolygonTrait, GI.MultiPolygonTrait}, geo
 
 end
 
+#=
+function _def_dim_var!(ds::AbstractDataset, dim::Dimension{<: GeometryLookup})
+    dimname = lowercase(string(DD.name(dim)))
+    haskey(ds.dim, dimname) && return nothing
+    CDM.defDim(ds, dimname, length(dim))
+    lookup(dim) isa NoLookup && return nothing
+    attribdict = _attribdict(NoMetadata())
 
 
+    CDM.defVar(ds, dimname, Vector(index(dim)), (dimname,); attrib=attrib)
+    
+end
+=#
 
 function _geometry_cf_decode(::Union{GI.PolygonTrait, GI.MultiPolygonTrait}, xs, ys, node_count_vec, part_node_count_vec, interior_ring_vec)
     current_xy_index = 1
