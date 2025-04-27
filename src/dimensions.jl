a0584fd9e7ab0f6c76774485667470758057063f
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-04-27 10:25:11 -0400

    add a Geometry dimension

--- src/dimensions.jl
+++ src/dimensions.jl
@@ -18,3 +18,29 @@ mean(A; dims=Band)
 ```
 """
 @dim Band
+
+
+"""
+    Geometry <: Dimension
+    Geometry(geoms)
+
+Geometry [`Dimension`]($DDdimdocs) for vector data cubes.
+
+"""
+@dim Geometry
+
+
+# Below is the example docstring for the Geometry dimension, which should be added back to the docs
+# when geometry lookups are fully implemented.
+#=
+## Example
+```julia
+geomdim = Geometry(GeometryLookup(polygons))
+# Or
+val = A[Geometry(1)]
+# Or
+val = A[Geometry(Touches(other_geom))] # this is automatically accelerated by spatial trees!
+# Or
+mean(A; dims=Geometry)
+```
+=#
