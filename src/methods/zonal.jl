3b052b9efd1be4ba5700d13232f592f6c2d7e00f
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-04-26 23:20:17 -0400

    Update zonal.jl

--- src/methods/zonal.jl
+++ src/methods/zonal.jl
@@ -147,7 +147,7 @@ function _zonal(f, x::RasterStackOrArray, ::Nothing, data;
     return_dimension = Dim{:Geometry}(axes(zs, 1))
     
     if zs isa AbstractVector{<: Union{<: AbstractDimArray, Missing}}
-        _cat_and_rebuild_parent(x, zs, return_dimension)
+        return _cat_and_rebuild_parent(x, zs, return_dimension)
     elseif zs isa AbstractVector{<: Union{<: AbstractDimStack, Missing}}
         dimarrays = NamedTuple{names(st)}(
             ntuple(length(names(st))) do i
