173eaa24693361e236a2a50b1308f8111c7fc47a
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-05-14 11:32:50 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -52,8 +52,7 @@ makedocs(
 # Enable logging to console again
 Logging.disable_logging(Logging.BelowMinLevel)
 
-deploydocs(; repo="github.com/rafaqz/Rasters.jl",
-    target = "build", # this is where Vitepress stores its output
+DocumenterVitepress.deploydocs(; repo="github.com/rafaqz/Rasters.jl",
     branch = "gh-pages",
     devbranch = "main",
     push_preview = true
